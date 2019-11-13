6885e9a85a305b842fd9384e7b88fad61772da6f
Jenny <ricejl@gmail.com>
2019-11-13 13:33:18 -0700

    start arrays

--- app/arrays.js
+++ app/arrays.js
@@ -4,11 +4,11 @@
 // output: ['is', 'a', 'split', 'sentence.', 'This']
 
 function rearranger(arr) {
+  arr.push(arr.shift());
+  return arr;
 }
 
-
 // ------------------------------------------
-
 
 // 2. Given an array of numbers, return the largest number in the array.
 // Example:
@@ -16,11 +16,10 @@ function rearranger(arr) {
 // output: 42
 
 function largestNum(arr) {
+  return Math.max(...arr);
 }
 
-
 // ------------------------------------------
-
 
 // 3. Given an array of numbers, return an array where every element in the given array is multiplied by the length of the given array.
 // Example:
@@ -28,11 +27,11 @@ function largestNum(arr) {
 // output: [16, 8, 4, 28]
 
 function elemsTimesLength(arr) {
+  let newArrValues = arr.map(x => x * arr.length);
+  return newArrValues;
 }
 
-
 // ------------------------------------------
-
 
 // 4. Given a multidimensional array, return a single dimension array with all of the primitive data elements within the multidimensional array.
 // Example:
@@ -40,48 +39,44 @@ function elemsTimesLength(arr) {
 // output: ['legume', 3, 2, 'tree', 5]
 // Primitive data types - https://developer.mozilla.org/en-US/docs/Glossary/Primitive
 
-function arrayFlattener(arr) {
-
-}
-
+function arrayFlattener(arr) {}
 
 // ------------------------------------------
-
 
 //5. Flights from Boise. Write a function that will use the following data to return the cost of flights from boise to a neighboring city, by default return the standard cost unless firstClass is set to true
 
-let flights = [{
-    from: 'BOI',
-    to: 'LAX',
+let flights = [
+  {
+    from: "BOI",
+    to: "LAX",
     prices: {
-        standard: 500,
-        firstClass: 2200
+      standard: 500,
+      firstClass: 2200
     }
-}, {
-    from: 'BOI',
-    to: 'SEA',
+  },
+  {
+    from: "BOI",
+    to: "SEA",
     prices: {
-        standard: 800,
-        firstClass: 1200
+      standard: 800,
+      firstClass: 1200
     }
-}, {
-    from: 'BOI',
-    to: 'CAN',
+  },
+  {
+    from: "BOI",
+    to: "CAN",
     prices: {
-        standard: 750,
-        firstClass: 6200
+      standard: 750,
+      firstClass: 6200
     }
-}]
-
+  }
+];
 
 function flightCost(destination, firstClass) {
-    //***hint: use the find method***
-
+  //***hint: use the find method***
 }
 
-
 // ------------------------------------------
-
 
 // 6. Given a number, return the corresponding user object from the staff array that has the given number as the value of their id property. If no user is found, return an object with an error property and value of "No user with that id."
 // Example:
@@ -89,20 +84,22 @@ function flightCost(destination, firstClass) {
 // output: {id: 17, name: 'St. MaryLou de la Playa Carmen'}
 //Example:
 // input: 1000
-// output: { error: "No user with that id." } 
+// output: { error: "No user with that id." }
 
+let staff = [
+  { id: 1, name: "Jon" },
+  { id: 2, name: "Yuli" },
+  { id: 21, name: "Peter" },
+  { id: 17, name: "St. MaryLou de la Playa Carmen" },
+  { id: 51, name: "Doug" },
+  { id: 881, name: "Paul" },
+  { id: 0, name: "Jon" },
+  { id: 999, name: "Timma" }
+];
 
-let staff = [{ id: 1, name: 'Jon' }, { id: 2, name: 'Yuli' }, { id: 21, name: 'Peter' },
-{ id: 17, name: 'St. MaryLou de la Playa Carmen' }, { id: 51, name: 'Doug' },
-{ id: 881, name: 'Paul' }, { id: 0, name: 'Jon' }, { id: 999, name: 'Timma' }]
-
-function findById(id) {
-
-}
-
+function findById(id) {}
 
 // ------------------------------------------
-
 
 // 7. Write a function that accepts a name argument and will loop over theBand members and return the band member's name and the instrument that he/she plays. Use string concatenation or interpolation to return a sentence with the following structure: "[band-members-name] is in the band and plays the [band-members-instrument]".
 // Example:
@@ -110,18 +107,21 @@ function findById(id) {
 // output: "Johnny P is in the band and plays the sax"
 
 let theBand = {
-    homeCity: "Birmingham",
-    members: [{
-        name: 'Johnny P',
-        instrument: 'sax'
-    }, {
-        name: 'River',
-        instrument: 'drums'
-    }, {
-        name: 'Kris',
-        instrument: 'guitar'
-    }]
-}
+  homeCity: "Birmingham",
+  members: [
+    {
+      name: "Johnny P",
+      instrument: "sax"
+    },
+    {
+      name: "River",
+      instrument: "drums"
+    },
+    {
+      name: "Kris",
+      instrument: "guitar"
+    }
+  ]
+};
 
-function bandMemberDetails(name) {
-}
+function bandMemberDetails(name) {}
